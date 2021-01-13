7941761d92454d897b81827a4d0733acbe2a0ba1
endihyan <ndihyan@gmail.com>
2021-01-13 23:35:02 +0700

    Add files via upload

---
+++ steamlogin.bat
@@ -0,0 +1,5 @@
+@echo off
+set username="INSERTYOURUSERNAMEHERE"
+reg add "HKCU\Software\Valve\Steam" /v AutoLoginUser /t REG_SZ /d %username% /f
+reg add "HKCU\Software\Valve\Steam" /v RememberPassword /t REG_DWORD /d 1 /f
+start steam://open/main
